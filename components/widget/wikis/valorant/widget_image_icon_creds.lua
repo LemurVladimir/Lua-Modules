11efa70cc5ac44c6b41299c2ab87f6077ff09f25
ElectricalBoy <15651807+ElectricalBoy@users.noreply.github.com>
2025-03-06 21:20:53 +0900

    slice out creds icon as widget

---
+++ components/widget/wikis/valorant/widget_image_icon_creds.lua
@@ -0,0 +1,34 @@
+---
+-- @Liquipedia
+-- wiki=valorant
+-- page=Module:Widget/Image/Icon/Creds
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+local Class = require('Module:Class')
+local Lua = require('Module:Lua')
+
+local Widget = Lua.import('Module:Widget')
+local HtmlWidgets = Lua.import('Module:Widget/Html/All')
+local IconImageWidget = Lua.import('Module:Widget/Image/Icon/Image')
+
+---@class CredsIconWidget: Widget
+local CredsIconWidget = Class.new(Widget)
+
+---@return Widget
+function CredsIconWidget:render()
+	return HtmlWidgets.Span{
+		css = { ['white-space'] = 'nowrap'},
+		children = {
+			IconImageWidget{
+				imageLight = 'Black_Creds_VALORANT.png',
+				imageDark = 'White_Creds_VALORANT.png',
+				link = 'Creds',
+				size = '10px'
+			}
+		}
+	}
+end
+
+return CredsIconWidget
